06b60a6c7c639ddbae40346fcfea78e01dfe9fb1
Dhruv Mittal <dhruvm14@gmail.com>
2021-02-05 00:59:26 +0530

    Header Changed

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,49 +1,76 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "../../axios";
 import "./Header.css";
+import { Swiper, SwiperSlide } from "swiper/react";
+import SwiperCore, {
+  Navigation,
+  Pagination,
+  Scrollbar,
+  A11y,
+  EffectCube,
+  Autoplay,
+  EffectCoverflow,
+} from "swiper";
+import "swiper/swiper.scss";
+import "swiper/components/navigation/navigation.scss";
+import "swiper/components/pagination/pagination.scss";
+import "swiper/components/scrollbar/scrollbar.scss";
+import "swiper/components/effect-coverflow/effect-coverflow.scss";
+
 const imgBase = "https://image.tmdb.org/t/p/original";
 
-export default class Header extends Component {
-  state = {
-    moviesData: [],
-    selectedMovie: {},
-  };
-  async componentDidMount() {
-    const moviesData = await axios.get(this.props.endPoint);
-    this.setState({ moviesData: moviesData.data.results });
-    const selectedMovieIndex = Math.floor(
-      Math.random() * this.state.moviesData.length
-    );
-    this.setState({
-      selectedMovie: this.state.moviesData[selectedMovieIndex],
-    });
-  }
+SwiperCore.use([
+  Navigation,
+  Pagination,
+  Scrollbar,
+  A11y,
+  EffectCube,
+  Autoplay,
+  EffectCoverflow,
+]);
 
-  render() {
-    return (
-      <div
-        className="Header"
-        style={{
-          backgroundImage: `url(
-            ${imgBase}${this.state.selectedMovie.backdrop_path}
-          )`,
-        }}
-      >
-        <div className="Header__Content">
-          <h1>
-            {this.state.selectedMovie?.title ||
-              this.state.selectedMovie?.original_title}
-          </h1>
-          <p>
-            {this.state.selectedMovie.overview &&
-              this.state.selectedMovie.overview.substr(0, 150)}
-            ...
-          </p>
-          <button>Play</button>
-          <button>More Info</button>
-        </div>
-        <div className="Header__bottom"></div>
-      </div>
-    );
-  }
+function Header(props) {
+  const [movies, setmovies] = useState([]);
+
+  useEffect(() => {
+    axios.get(props.endPoint).then((data) => {
+      setmovies(data.data.results);
+      console.log(data.data.results);
+    });
+    // const getData = async () => {
+    //   const moviesData = await axios.get(props.endPoint);
+    //   setmovies(moviesData.data.results);
+    //   console.log(movies);
+    // };
+    // getData();
+  }, [props.endPoint]);
+
+  return (
+    <div className="Header">
+      {movies.length > 0 && (
+        <Swiper
+          className="main-swiper"
+          spaceBetween={0}
+          navigation
+          slidesPerView={3}
+          effectCube
+          autoplay
+          effect="coverflow"
+          // onSlideChange={() => console.log("slide change")}
+          // onSwiper={(swiper) => console.log(swiper)}
+        >
+          {movies.map((movie) => (
+            <SwiperSlide
+              className="slide"
+              style={{
+                backgroundImage: `url(${imgBase}${movie.poster_path})`,
+              }}
+            ></SwiperSlide>
+          ))}
+        </Swiper>
+      )}
+    </div>
+  );
 }
+
+export default Header;
